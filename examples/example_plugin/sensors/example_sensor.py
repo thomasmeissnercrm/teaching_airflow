45a4284199dc2cd6da7a72c57ec5f5de226c1657
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-10 17:34:29 +0200

    Add examples

--- examples/example_plugin/sensors/example_sensor.py
+++ examples/example_plugin/sensors/example_sensor.py
@@ -3,13 +3,22 @@ import logging
 
 
 class ExampleSensor(BaseSensorOperator):
+    """
+    Put here comment of how sensor is working and parameters that is using
+    :param my_parameter str -> ex. connection id used in sensor
+    """
     def __init__(self, my_parameter, *args, **kwargs):
         self.my_parameter = my_parameter
         super(BaseSensorOperator).__init__(*args, **kwargs)
 
     def poke(self, context):
-        logging.info('Sensors requaired poke method as it will be executed in pipeline')
-        logging.info('depending on needed contidion, its need to return True of False value')
+        """
+        Each method also should have comment of what happend inside
+        :param context:
+        :return:
+        """
+        logging.info('Sensors required poke method as it will be executed in pipeline')
+        logging.info('depending on needed condition, its need to return True of False value')
 
         if self.my_parameter is True:
             return True
