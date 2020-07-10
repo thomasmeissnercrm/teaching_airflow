4a2d94c0873a274a8ea9e3adaa3a59d2e8d9d787
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-10 17:23:39 +0200

    Add examples

--- plugins/postgres_extended_plugin/hooks/postgres_extended_hook.py
+++ plugins/postgres_extended_plugin/hooks/postgres_extended_hook.py
@@ -1,4 +1,18 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from contextlib import closing
+import sys
+
 
 class PostgresExtendedHook(PostgresHook):
-    pass
+
+    def __init__(self, conn_id, *args, **kwargs):
+        self.conn_id = conn_id
+        super(PostgresHook).__init__(*args, **kwargs)
+
+    def get_pandas_df(self, sql, parameters=None, chunk_size=None):
+        if sys.version_info[0] < 3:
+            sql = sql.encode('utf-8')
+        import pandas.io.sql as psql
+
+        with closing(self.get_conn()) as conn:
+            return psql.read_sql(sql, con=conn, chunksize=chunk_size, params=parameters)
