35fdcb42a5feaee87dd1af046b26e34e42e43526
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-10 17:00:17 +0200

    Add examples

--- examples/example_plugin/operators/example_operator.py
+++ examples/example_plugin/operators/example_operator.py
@@ -2,11 +2,16 @@ from airflow.operators import BaseOperator
 import logging
 
 
-class MyOperator(BaseOperator):
+class ExampleOperator(BaseOperator):
     def __init__(self, my_parameter, *args, **kwargs):
-        self.my_paramter = my_parameter
+        self.my_parameter = my_parameter
         super(BaseOperator).__init__(*args, **kwargs)
+
+    @staticmethod
+    def additional_method(self):
+        logging.info('You can also put as many methods as needed. Also static one')
 
     def execute(self, context):
         logging.info('Here is your code that will be executed by operator')
+        logging.info('the execute method is obligatory to implement in operator!')
 
