1b98083cbd26cb1cbcfedee1b4f9df8625b21a20
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-10 17:40:45 +0200

    Add examples

--- examples/example_plugin/__init__.py
+++ examples/example_plugin/__init__.py
@@ -1,3 +1,8 @@
+"""
+Here is important part.
+We want to let airflow easy know what plugins are ready to use in dags durning execution.
+In that case we need to 'register them'. This always happen in __init__ file of plugin folder.
+"""
 from airflow.plugins_manager import AirflowPlugin
 from example_plugin.operators.example_operator import ExampleOperator
 from example_plugin.hooks.example_hook import ExampleHook
