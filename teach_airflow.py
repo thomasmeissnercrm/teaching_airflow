408dad8af08d5b12c9a697cdb88404374546316d
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-15 23:19:15 +0200

    Adjustment

--- teach_airflow.py
+++ teach_airflow.py
@@ -13,8 +13,8 @@ os.system(f'docker build {path_docker_airflow} -t teaching_airflow:main')
 
 print('Rise env')
 os.system('docker-compose -f airflow.yaml up -d')
-print('Pause for 10 - databases need to start.')
-time.sleep(10)
+print('Pause for 60 - databases need to start.')
+time.sleep(60)
 print('Load example data')
 os.system('docker run -it --rm --name my-running-script --network=teaching_airflow_default '
-          '-v "$PWD":/usr/src/myapp -w /usr/src/myapp teaching_python:3 python data_loader.py')
+          f'-v {Path.cwd()}:/usr/src/myapp -w /usr/src/myapp teaching_python:3 python data_loader.py')
