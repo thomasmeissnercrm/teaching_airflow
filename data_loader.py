e3b2b894fb6614e8071897a0c18b11cf86420dff
Rafal Chmielewski <rafal.chmielewski.rch@gmail.com>
2020-07-15 20:47:28 +0200

    add example data and loader.

---
+++ data_loader.py
@@ -0,0 +1,57 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+
+# create list of files to import
+cwd = os.getcwd()
+table_list = []
+print('Loading list of files to upload in db.')
+for path, subdirs, files in os.walk(f'{os.getcwd()}/example_data'):
+    for name in files:
+        data = os.path.join(path, name)
+        data = data.split('/')
+        system = data[-3]
+        schema = data[-2]
+        table = data[-1].split('.')[0]
+        ext = data[-1].split('.')[-1]
+        if system != 'teaching_airflow':
+            table_list.append({
+                "engine": system,
+                "schema": schema,
+                "table": table,
+                "ext": ext
+            })
+
+print(f'Load done. Found {len(table_list)} files to upload.')
+print('generating db engines')
+psql = create_engine('postgresql://airflow:airflow@localhost:5432')
+mysql = create_engine('mysql://root:airflow@127.0.0.1:3306/public')
+
+# getting list of schema in mysql
+mysql_schema = pd.read_sql('select SCHEMA_NAME AS "schema" from information_schema.SCHEMATA;', con=mysql)['schema'].to_list()
+psql_schema = pd.read_sql('Select schema_name as schema from information_schema.schemata;', con=psql)['schema'].to_list()
+
+print('Loading data into databases')
+
+for element in table_list:
+    df = pd.read_csv(f'{cwd}/example_data/{element["engine"]}/{element["schema"]}/{element["table"]}.{element["ext"]}')
+    db_engine = psql if element['engine'] == 'psql' else mysql
+    if element['engine'] == 'mysql':
+        if element["schema"] not in mysql_schema:
+            mysql.execute(f"create schema {element['schema']}; ")
+            mysql.execute(f"GRANT ALL PRIVILEGES ON {element['schema']}.* TO 'airflow'@'%';")
+            mysql_schema.append(element["schema"])
+    else:
+        if element["schema"] not in psql_schema:
+            psql.execute(f"create schema {element['schema']}; ")
+            psql_schema.append(element["schema"])
+
+    df.to_sql(
+        name=element['table'],
+        schema=element['schema'],
+        con=db_engine,
+        if_exists='replace'
+    )
+    print(f'Loaded table {element["schema"]}.{element["table"]} into {element["engine"]}')
+
+print('Load done.')
